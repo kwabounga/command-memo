ecdd9f4c8b24a26e516b2b365bb302bbff2c340e
jean-yves <jean-yves@moulinroty.fr>
2026-02-05 17:23:06 +0100

    wip work on css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,33 +11,31 @@
         --nb-col: 4;
     }
 
-    :global {
-        .main-wrapper {
-            background: transparent;
-            /* background: #00000061;*/
-            max-height: calc(100vh - var(--offset-main)) !important;
-            overflow: hidden;
-        }
+    :global(.main-wrapper) {
+        background: transparent;
+        /* background: #00000061;*/
+        max-height: calc(100vh - var(--offset-main)) !important;
+        overflow: hidden;
+    }
 
-        .command-grid {
-            display: flex;
-            flex-direction: column;
-            flex-wrap: wrap;
+    :global(.command-grid) {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
 
-            height: calc(100vh - var(--offset)); /* hauteur connue */
-            gap: 0.5rem;
-            align-content: flex-start;
+        height: calc(100vh - var(--offset)); /* hauteur connue */
+        gap: 0.5rem;
+        align-content: flex-start;
 
-            overflow: auto;
-        }
+        overflow: auto;
+    }
 
-        .command-group {
-            width: calc(100% / var(--nb-col) - 0.5rem); /* 4 colonnes */
-        }
+    :global(.command-group) {
+        width: calc(100% / var(--nb-col) - 0.5rem); /* 4 colonnes */
+    }
 
-        .commands-list {
-            overflow-y: auto;
-            max-height: 25rem;
-        }
+    :global(.commands-list) {
+        overflow-y: auto;
+        max-height: 25rem;
     }
 </style>
