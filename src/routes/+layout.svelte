e22b32e3cef1b3f0e84391b47a342a9796d82482
jean-yves <jean-yves@moulinroty.fr>
2026-02-05 17:17:47 +0100

    tray to move css in layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import {Styles} from '@sveltestrap/sveltestrap';
+</script>
+
+<Styles theme="dark"/>
+<slot />
+<style>
+    :root {
+        --offset-main: 75px;
+        --offset: 140px;
+        --nb-col: 4;
+    }
+
+    :global {
+        .main-wrapper {
+            background: transparent;
+            /* background: #00000061;*/
+            max-height: calc(100vh - var(--offset-main)) !important;
+            overflow: hidden;
+        }
+
+        .command-grid {
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+
+            height: calc(100vh - var(--offset)); /* hauteur connue */
+            gap: 0.5rem;
+            align-content: flex-start;
+
+            overflow: auto;
+        }
+
+        .command-group {
+            width: calc(100% / var(--nb-col) - 0.5rem); /* 4 colonnes */
+        }
+
+        .commands-list {
+            overflow-y: auto;
+            max-height: 25rem;
+        }
+    }
+</style>
